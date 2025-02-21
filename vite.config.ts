4853d4b07b22a43a9c64e0c831395922c665e0b1
shz-code <web.shahidul.alam@gmail.com>
2025-02-21 22:54:17 +0600

    modify: downgrade to tailwind@3 for shadcn compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), vueDevTools(), tailwindcss()],
+  plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
