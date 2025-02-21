694fa9cb05e601def5146b0c702ae3f7d188777f
shz-code <web.shahidul.alam@gmail.com>
2025-02-21 23:09:28 +0600

    add: shadcn

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AppButton from './components/ui/button/AppButton.vue'
+</script>
 
 <template>
-  <header>Hello to my spenz</header>
+  <header class="bg-slate-800 text-white">Hello to my spenz</header>
 
   <nav>
     <router-link :to="{ name: 'Home' }">Home</router-link>
     <router-link :to="{ name: 'About' }">About</router-link>
   </nav>
+
+  <AppButton>Shadcn Button</AppButton>
   <main>
     <RouterView />
   </main>
